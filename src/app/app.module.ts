c0b00bf9283b2e9a44d14c515be468d0c4b85a46
Julio <juliansal.slc@gmail.com>
2018-06-22 00:52:20 -0700

    add more styles and prismjs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,14 @@
 import { element } from 'protractor';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+
+import 'prismjs/prism';
+
+/* Import the language you need to highlight */
+import 'prismjs/components/prism-typescript';
+
+import { PrismComponent } from 'angular-prism';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -10,13 +17,15 @@ import { appRouting } from './/app-routing.module';
 import { SkillsbarComponent } from './skillsbar/skillsbar.component';
 import { LatestArticlesComponent } from './latest-articles/latest-articles.component';
 
+
 @NgModule({
 	declarations: [
 		AppComponent,
 		HomeComponent,
 		ContactComponent,
 		SkillsbarComponent,
-		LatestArticlesComponent
+		LatestArticlesComponent,
+		PrismComponent
 	],
 	imports: [
 		BrowserModule,
@@ -24,6 +33,7 @@ import { LatestArticlesComponent } from './latest-articles/latest-articles.compo
 		appRouting
 	],
 	providers: [],
-	bootstrap: [AppComponent]
+	bootstrap: [AppComponent],
+	schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,8 +6,14 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+	tsCode: string;
 
-	constructor() { }
+	constructor() {
+		this.tsCode = `console.log('hello world');`;
+
+	}
+
+	code = `console.log("how are you?");`.trim();
 
 	ngOnInit() {
 	}
